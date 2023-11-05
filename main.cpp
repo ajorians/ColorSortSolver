8420d08c26fc994b5fa43903c68d294f2fc9b61f
A.J. Orians <ajorians@gmail.com>
2023-11-04 22:36:53 -0400

    Starting changes.

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main()
+{
+    int numTubes;
+   std::cout << "Enter number of tubes:";
+   std::cin >> numTubes;
+
+   int numBallsPerTube;
+   std::cout << "Enter number of balls per tube:"
+   std::cin >> numBallsPerTube;
+}
